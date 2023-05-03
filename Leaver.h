fbe9e8306d711a2997980569bc3ca2fb31173751
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-03 19:22:02 +0300

    Initial commit

---
+++ Leaver.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+#ifdef LEAVERDYNAMICLIBRARY_EXPORTS
+#define LEAVERDYNAMICLIB_API __declspec(dllexport)
+#else
+#define LEAVERDYNAMICLIB_API __declspec(dllimport)
+#endif
+
+namespace Leaver
+{
+	class Leaver
+	{
+		//private:
+			//std::string leavingPerson_;
+
+	public:
+		// Leaver(std::string nameOfPerson);
+
+		LEAVERDYNAMICLIB_API std::string leave(std::string nameOfPerson);
+	};
+}
